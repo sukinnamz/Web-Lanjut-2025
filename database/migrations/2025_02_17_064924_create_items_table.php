b0fe238578dbdf9aeaf0d6c66e6add1132e9ae7d
sukinnamz <innama.maesaputri@gmail.com>
2025-02-17 15:53:17 +0700

    add : komen penjelasan migration

--- database/migrations/2025_02_17_064924_create_items_table.php
+++ database/migrations/2025_02_17_064924_create_items_table.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+//Ini adalah kelas dasar untuk migration di Laravel. Setiap migration harus mewarisi kelas ini.
 use Illuminate\Database\Schema\Blueprint;
+//Digunakan untuk mendefinisikan struktur tabel dalam migration.
 use Illuminate\Support\Facades\Schema;
+//Menyediakan fungsionalitas untuk operasi schema seperti membuat, mengubah, atau menghapus tabel.
 
 return new class extends Migration {
     /**
@@ -11,10 +14,16 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('items', function (Blueprint $table) {
+            //Membuat tabel bernama items.
+            //Parameter Blueprint $table digunakan untuk mendefinisikan kolom tabel.
             $table->id();
+            //Menambahkan kolom id sebagai primary key dengan auto-increment.
             $table->string('name');
+            //Menambahkan kolom name dengan tipe data string (biasanya untuk nama item).
             $table->string('description');
+            //Menambahkan kolom description untuk menyimpan deskripsi item, juga bertipe string.
             $table->timestamps();
+            //Menambahkan dua kolom waktu: created_at dan updated_at secara otomatis oleh Laravel. Kolom ini akan mencatat waktu pembuatan dan pembaruan data.
         });
     }
 
@@ -24,5 +33,6 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::dropIfExists('items');
+        //Menghapus tabel items jika tabel tersebut ada.
     }
 };
