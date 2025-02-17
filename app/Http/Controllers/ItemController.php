2e02df7e1281939eb995028262db923b6530248a
sukinnamz <innama.maesaputri@gmail.com>
2025-02-17 14:11:48 +0700

    update : controller

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
@@ -11,7 +12,8 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        $items = Item::all();
+        return view('item.index', compact('items'));
     }
 
     /**
@@ -19,7 +21,7 @@ class ItemController extends Controller
      */
     public function create()
     {
-        //
+        return view('item.create');
     }
 
     /**
@@ -27,38 +29,58 @@ class ItemController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+        ]);
+
+        //Item::create($request->all());
+        //return redirect()->route('items.index');
+
+        // Hanya masukkan atribut yang diizinkan
+        Item::create($request->only(['name', 'description']));
+        return redirect()->route('items.index')->with('success', 'Item added successfully.');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Item $item)
     {
-        //
+        return view('items.show', compact('item'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Item $item)
     {
-        //
+        return view('items.edit', compact('item'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Item $item)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+        ]);
+
+        //$item->update($request->all());
+        //return redirect()->route('items.index');
+        // Hanya masukkan atribut yang diizinkan
+        $item->update($request->only(['name', 'description']));
+        return redirect()->route('items.index')->with('success', 'Item updated successfully.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Item $item)
     {
-        //
+        $item->delete();
+        return redirect()->route('items.index')->with('success', 'Item deleted successfully.');
     }
 }
