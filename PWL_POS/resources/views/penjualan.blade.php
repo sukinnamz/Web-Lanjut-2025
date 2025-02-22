1c26aab604648415817d21f684a634f4d1f7a2e4
sukinnamz <innama.maesaputri@gmail.com>
2025-02-22 21:40:10 +0700

    add : Tugas Praktikum Jobsheet 2

---
+++ PWL_POS/resources/views/penjualan.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Halaman Penjualan</title>
+</head>
+
+<body>
+    <h1>Sistem POS - Penjualan</h1>
+</body>
+
+</html>
--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PenjualanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index']);
+
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductsController::class, 'foodBeverage']);
+    Route::get('/beauty-health', [ProductsController::class, 'beautyHealth']);
+    Route::get('/home-care', [ProductsController::class, 'homeCare']);
+    Route::get('/baby-kid', [ProductsController::class, 'babyKid']);
 });
+
+Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
+
+Route::get('/penjualan', [PenjualanController::class, 'index']);
