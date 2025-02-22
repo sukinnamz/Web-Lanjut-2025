808cd4efbbbe12d1f849e1ab43abf35310c5246e
sukinnamz <innama.maesaputri@gmail.com>
2025-02-22 16:48:31 +0700

    add : basic routing

--- PWL_2025/routes/web.php
+++ PWL_2025/routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM : 2341720235 <br>Nama : Innama Maesa Putri';
 });
