32ddef2401fbde73fe7c7dc4f00b8552840f8dc1
sukinnamz <innama.maesaputri@gmail.com>
2025-03-21 18:23:04 +0700

    add : JS 5 Baru Praktikum 3

--- Pertemuan05-baru/PWL_POS/routes/web.php
+++ Pertemuan05-baru/PWL_POS/routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductsController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\PenjualanController;
-use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,23 +19,75 @@ use App\Http\Controllers\WelcomeController;
 |
 */
 
-// Route::get('/', [HomeController::class, 'index']);
-
-// Route::prefix('category')->group(function () {
-//     Route::get('/food-beverage', [ProductsController::class, 'foodBeverage']);
-//     Route::get('/beauty-health', [ProductsController::class, 'beautyHealth']);
-//     Route::get('/home-care', [ProductsController::class, 'homeCare']);
-//     Route::get('/baby-kid', [ProductsController::class, 'babyKid']);
+// Route::get('/', function () {
+//     return view('welcome');
 // });
 
-// Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
-
-// Route::get('/penjualan', [PenjualanController::class, 'index']);
-
-// Route::get('/level', [LevelController::class, 'index']);
-
-// Route::get('/kategori', [KategoriController::class, 'index']);
-
-// Route::get('/user', [UserController::class, 'index']);
-
 Route::get('/', [WelcomeController::class, 'index']);
+
+// Route::get('/level',[LevelController::class,'index']);
+// Route::get('/kategori',[KategoriController::class,'index']);
+// Route::get('/user',[UserController::class,'index']);
+
+// Route::get('/user/tambah',[UserController::class,'tambah']);
+// Route::get('/user/tambah_simpan',[UserController::class,'tambah_simpan']);
+
+// Route::get('/user/ubah/{id}',[UserController::class,'ubah']);
+// Route::put('/user/ubah_simpan/{id}',[UserController::class,'ubah_simpan']);
+
+// Route::get('/user/hapus/{id}',[UserController::class,'hapus']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/list', [UserController::class, 'list']);
+    Route::get('/create', [UserController::class, 'create']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::get('/{id}/edit', [UserController::class, 'edit']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'level'], function () {
+    Route::get('/', [LevelController::class, 'index']);
+    Route::post('/list', [LevelController::class, 'list']);
+    Route::get('/create', [LevelController::class, 'create']);
+    Route::post('/', [LevelController::class, 'store']);
+    Route::get('/{id}', [LevelController::class, 'show']);
+    Route::get('/{id}/edit', [LevelController::class, 'edit']);
+    Route::put('/{id}', [LevelController::class, 'update']);
+    Route::delete('/{id}', [LevelController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'kategori'], function () {
+    Route::get('/', [KategoriController::class, 'index']);
+    Route::post('/list', [KategoriController::class, 'list']);
+    Route::get('/create', [KategoriController::class, 'create']);
+    Route::post('/', [KategoriController::class, 'store']);
+    Route::get('/{id}', [KategoriController::class, 'show']);
+    Route::get('/{id}/edit', [KategoriController::class, 'edit']);
+    Route::put('/{id}', [KategoriController::class, 'update']);
+    Route::delete('/{id}', [KategoriController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'barang'], function () {
+    Route::get('/', [BarangController::class, 'index']);
+    Route::post('/list', [BarangController::class, 'list']);
+    Route::get('/create', [BarangController::class, 'create']);
+    Route::post('/', [BarangController::class, 'store']);
+    Route::get('/{id}', [BarangController::class, 'show']);
+    Route::get('/{id}/edit', [BarangController::class, 'edit']);
+    Route::put('/{id}', [BarangController::class, 'update']);
+    Route::delete('/{id}', [BarangController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'supplier'], function () {
+    Route::get('/', [SupplierController::class, 'index']);
+    Route::post('/list', [SupplierController::class, 'list']);
+    Route::get('/create', [SupplierController::class, 'create']);
+    Route::post('/', [SupplierController::class, 'store']);
+    Route::get('/{id}', [SupplierController::class, 'show']);
+    Route::get('/{id}/edit', [SupplierController::class, 'edit']);
+    Route::put('/{id}', [SupplierController::class, 'update']);
+    Route::delete('/{id}', [SupplierController::class, 'destroy']);
+});
