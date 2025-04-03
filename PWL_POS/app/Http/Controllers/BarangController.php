fad4d81c0fc2b4556f2aca8fc16cb1b5f7ccfde4
sukinnamz <innama.maesaputri@gmail.com>
2025-04-03 07:23:04 +0700

    add : jobsheet 8 praktikum 3

--- PWL_POS/app/Http/Controllers/BarangController.php
+++ PWL_POS/app/Http/Controllers/BarangController.php
@@ -419,4 +419,21 @@ class BarangController extends Controller
         exit;
 
     }
+
+    public function export_pdf()
+    {
+        $barang = BarangModel::select('kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual')
+            ->orderBy('kategori_id')
+            ->orderBy('barang_kode')
+            ->with('kategori')
+            ->get();
+
+        $pdf = Pdf::loadView('barang.export_pdf', ['barang' => $barang]);
+        $pdf->setPaper('a4', 'portrait'); // set ukuran kertas dan orientasi
+        $pdf->setOption("isRemoteEnabled", true); // set true jika ada gambar dari url
+        $pdf->render();
+
+        return $pdf->stream('Data Barang ' . date('Y-m-d H:i:s') . '.pdf');
+    }
+
 }
