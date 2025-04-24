30ee9761c59aa2b28b26b8dafa273fc9912a0d86
sukinnamz <innama.maesaputri@gmail.com>
2025-04-24 14:13:33 +0700

    add : jobsheet 10 praktikum 2

--- PWL_POS/routes/api.php
+++ PWL_POS/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,7 @@ use App\Http\Controllers\Api\RegisterController;
 // });
 
 Route::post('/register', RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function(Request $request) {
+    return $request->user();
+});
